06ac5835e0b0a877f736bff28d039d4ad08b3ec3
Joary13 <jolea13008@gmail.com>
2024-03-13 14:23:15 +0100

    responsive design

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="text-sm font-semibold">Joary</div>;
+  return <div className="hidden text-sm font-semibold">Joary</div>;
 }
 
 export default UserName;
