c62784d42277520e3dfd575452f34c13ff3d8c6a
Joary13 <jolea13008@gmail.com>
2024-03-29 17:53:28 +0100

    redux Thunks with createAsyncThunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,11 +4,12 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
 import EmptyCart from '../cart/EmptyCart';
+import { fetchAdress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -45,6 +46,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
+  const dispatch = useDispatch();
   const cart = useSelector(getCart);
   console.log(cart);
   const username = useSelector((state) => state.user.username);
@@ -58,7 +60,7 @@ function CreateOrder() {
       <h2 className="font mb-8 text-xl font-semibold">
         Ready to order? Let's go!
       </h2>
-
+      <button onClick={() => dispatch(fetchAdress())}>Get position</button>
       {/* <Form method='POST' action="/order/new"> */}
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
